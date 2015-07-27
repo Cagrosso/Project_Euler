849f26c0fa5022a2fb4d043965559b9ccb747091
Cameron Panagrosso <cameron.panagrosso@gmail.com>
2015-07-26 22:24:00 -0400

    Add loops to check i numbers

--- euler1/euler1.py
+++ euler1/euler1.py
@@ -1,5 +1,10 @@
 #If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #Find the sum of all the multiples of 3 or 5 below 1000.
+multiples = []
+total = 0;
+
+print("Finds the sum of all multiples of 3 or 5 below a max number. \n")
+limit = input("Max number? ")
 
 def isMult(x):
     if(x % 3 == 0 or x % 5 == 0):
@@ -7,6 +12,13 @@ def isMult(x):
     else:
         return False
 
-print('3: ', isMult(3))
-print('4: ', isMult(4))
-print('5: ', isMult(5))
+#print('3: ', isMult(3))
+#print('4: ', isMult(4))
+#print('5: ', isMult(5))
+
+for i in range(0, limit):
+    if(isMult(i)):
+        multiples.append(i)
+
+for i in range(0, multiples.length):
+    total += i
